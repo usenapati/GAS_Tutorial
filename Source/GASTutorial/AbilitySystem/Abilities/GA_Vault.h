8e0330d8517a763124f2c70fb9796b15c9ce7f3a
Udayan Senapati <udayansenapati@gmail.com>
2025-08-22 13:58:05 -0400

    WIP Vault

---
+++ Source/GASTutorial/AbilitySystem/Abilities/GA_Vault.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GAS_GameplayAbility.h"
+#include "GA_Vault.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASTUTORIAL_API UGA_Vault : public UGAS_GameplayAbility
+{
+	GENERATED_BODY()
+	
+};
