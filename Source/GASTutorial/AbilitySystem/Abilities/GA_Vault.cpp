4703ebfeee030e1e8ff2ac12fd8aa30fbe8603ef
Udayan Senapati <udayansenapati@gmail.com>
2025-09-04 01:29:07 -0400

    WIP Vault - Commit Check

--- Source/GASTutorial/AbilitySystem/Abilities/GA_Vault.cpp
+++ Source/GASTutorial/AbilitySystem/Abilities/GA_Vault.cpp
@@ -20,6 +20,51 @@ UGA_Vault::UGA_Vault()
 bool UGA_Vault::CommitCheck(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo, FGameplayTagContainer* OptionalRelevantTags)
 {
+	if (!Super::CommitCheck(Handle, ActorInfo, ActivationInfo, OptionalRelevantTags))
+	{
+		return false;
+	}
+
+	AGAS_Character* Character = GetGasCharacterFromActorInfo();
+
+	if (!IsValid(Character))
+	{
+		return false;
+	}
+
+	const FVector StartLocation = Character->GetActorLocation();
+	const FVector ForwardVector = Character->GetActorForwardVector();
+	const FVector UpVector = Character->GetActorUpVector();
+
+	TArray<AActor*> ActorsToIgnore = {Character};
+
+	static const auto CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("ShowDebugTraversal"));
+	const bool bShowTraversal = CVar->GetInt() > 0;
+
+	EDrawDebugTrace::Type DebugDrawType = bShowTraversal ? EDrawDebugTrace::ForDuration : EDrawDebugTrace::None;
+
+	bool bJumpToLocationSet = false;
+
+	int32 JumpToLocationIndex = INDEX_NONE;
+
+	int i = 0;
+
+	FHitResult TraceHit;
+
+	float MaxJumpDistance = HorizontalTraceLength;
+
+	for (; i < HorizontalTraceLength; ++i)
+	{
+		const FVector TraceStart = StartLocation + i * UpVector * HorizontalTraceStep;
+		const FVector TraceEnd = TraceStart + ForwardVector * HorizontalTraceLength;
+
+		if (UKismetSystemLibrary::SphereTraceSingleForObjects(this, TraceStart, TraceEnd, HorizontalTraceRadius, TraceObjectTypes, true, ActorsToIgnore, DebugDrawType, TraceHit, true))
+		{
+			
+		}
+	}
+	
+	
 	return Super::CommitCheck(Handle, ActorInfo, ActivationInfo, OptionalRelevantTags);
 }
 
